45722ad46ae986e3a14f34d7d896f5ab08815b03
noeyiz <lamoureclatant@gmail.com>
2023-11-28 02:05:16 +0900

    :sparkles: feat: 스토리보드 제거

---
+++ Reminders_UIKit/Reminders_UIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reminders_UIKit
+//
+//  Created by jiyeon on 11/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .systemBackground
+    }
+
+
+}
+
