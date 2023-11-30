c817fa863d29d340371e6beb0b95b5eea4f2ddb7
noeyiz <lamoureclatant@gmail.com>
2023-11-30 14:08:15 +0900

    :sparkles: feat: Reminder 모델 정의

---
+++ Reminders_UIKit/Reminders_UIKit/Models/Reminder.swift
@@ -0,0 +1,27 @@
+//
+//  Reminder.swift
+//  Reminders_UIKit
+//
+//  Created by jiyeon on 11/30/23.
+//
+
+import Foundation
+
+struct Reminder {
+    var id: UUID = UUID()
+    var title: String = ""
+    var notes: String?
+    var isCompleted: Bool = false
+    var priority: Priority = .none
+}
+
+extension Reminder {
+    
+    enum Priority {
+        case none
+        case low
+        case middle
+        case high
+    }
+    
+}
